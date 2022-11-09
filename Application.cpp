81da9ca385588af0c7221c2d607a2bc90c9c4996
Anmutiger Biber <l.p.m@gmx.de>
2022-11-09 21:37:09 +0100

    Enabled vertical sync in Application Class

--- Application.cpp
+++ Application.cpp
@@ -1,7 +1,5 @@
 #include "Application.h"
 
-#include <iostream>
-
 Application::~Application() {
 	delete this->stack;
 	
@@ -12,6 +10,7 @@ Application::Application(Context* c) {
 	this->context = c;
 
 	this->window = new sf::RenderWindow(sf::VideoMode(this->context->SCREEN_WIDTH, this->context->SCREEN_HEIGHT), "Strategy", sf::Style::Fullscreen);
+	this->window->setVerticalSyncEnabled(true);
 
 	this->stack = new StateStack(context);
 	this->stack->push(c->MAIN_MENU_STATE);
