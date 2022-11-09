773397574aad20e8234709a2e843972faaf1d990
Anmutiger Biber <l.p.m@gmx.de>
2022-11-09 13:55:55 +0100

    Added another GameState constant to Context class

--- Context.h
+++ Context.h
@@ -11,6 +11,8 @@ public:
 	const std::string MAIN_MENU_STATE = "Main Menu State";
 	const std::string MAIN_MENU_STATE_START_GAME_BUTTON = "MainMenuState.StartGame";
 
+	const std::string SINGLEPLAYER_GAME_STATE = "Singleplayer Game State";
+
 	const std::string SANFW_FONT = "assets/fonts/sanfw.ttf";
 };
 
